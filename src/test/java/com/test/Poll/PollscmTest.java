cf4ff5fcc4e5f10f5bca50234a178e3a561c6203
VAISHALI <VAISHALI@DESKTOP-1UFPC69>
2022-05-31 12:12:57 +0530

    Pollscm

---
+++ src/test/java/com/test/Poll/PollscmTest.java
@@ -0,0 +1,16 @@
+package com.test.Poll;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollscmTest 
+{
+		@Test
+			public void test2Test()
+			{
+			    Reporter.log("Test case1",true);
+				
+			}
+		}
+
+
